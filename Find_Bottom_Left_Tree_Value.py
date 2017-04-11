c33271df305688e0d0b71b766cf5f6c7aa4e7d83
hecter_wu <hecter_wu@transcend.com.tw>
2017-04-11 17:40:20 +0800

    pass case testCompleteBinaryTree by recursive

--- Find_Bottom_Left_Tree_Value.py
+++ Find_Bottom_Left_Tree_Value.py
@@ -1,14 +1,29 @@
 import unittest
 
+class TreeNode(object):
+    def __init__(self, x = None):
+        self.val = x
+        self.left = None
+        self.right = None
+
 class unitest(unittest.TestCase):
     def testNullRoot(self):
-        root = []
+        root = None
         self.assertEqual(Solution.findBottomLeftValue(self,root),None);
+    def testCompleteBinaryTree(self):
+        root = TreeNode(2)
+        root.left = TreeNode(1)
+        root.right = TreeNode(3)
+        self.assertEqual(Solution.findBottomLeftValue(self,root),1);
 
 class Solution():
     def findBottomLeftValue(self, root):
         if(root is None):
             return None
+        if(root.left is None):
+            return root.val
+        else:
+            return Solution.findBottomLeftValue(self,root.left)
 
 if __name__ == '__main__':
     unittest.main()
