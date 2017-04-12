982fdfc344f91e5de80172c1f80a5f90df1d98dc
hecter_wu <hecter_wu@transcend.com.tw>
2017-04-12 10:38:16 +0800

    pass case testBinaryTree by DFS

--- Find_Bottom_Left_Tree_Value.py
+++ Find_Bottom_Left_Tree_Value.py
@@ -1,4 +1,5 @@
 import unittest
+from operator import itemgetter
 
 class TreeNode(object):
     def __init__(self, x = None):
@@ -9,21 +10,31 @@ class TreeNode(object):
 class unitest(unittest.TestCase):
     def testNullRoot(self):
         root = None
-        self.assertEqual(Solution.findBottomLeftValue(self,root),None);
+        self.assertEqual(Solution().findBottomLeftValue(root),None);
     def testCompleteBinaryTree(self):
         root = TreeNode(2)
         root.left = TreeNode(1)
         root.right = TreeNode(3)
-        self.assertEqual(Solution.findBottomLeftValue(self,root),1);
+        self.assertEqual(Solution().findBottomLeftValue(root),1);
+    def testBinaryTree(self):
+        root = TreeNode(2)
+        root.left = TreeNode(1)
+        root.right = TreeNode(3)
+        root.left.right = TreeNode(5)
+        root.right.left = TreeNode(4)
+        self.assertEqual(Solution().findBottomLeftValue(root),5);
 
-class Solution():
+class Solution:
     def findBottomLeftValue(self, root):
         if(root is None):
             return None
-        if(root.left is None):
-            return root.val
-        else:
-            return Solution.findBottomLeftValue(self,root.left)
+        return self.DFS(root)[1]
+    def DFS(self, root):
+        if(root is None):
+            return (-1,0)
+        lis = [self.DFS(root.left),self.DFS(root.right),(0, root.val)]
+        (x,y) = max(lis,key=lambda item:item[0])
+        return (x+1,y)
 
 if __name__ == '__main__':
     unittest.main()
